35f306279ad753177d84bdcc856290761928f554
Ivan Lyutov <iv.lyutov@gmail.com>
2013-08-07 17:14:13 +0300

    merchant_name_descriptor support

--- lib/spreedly-core-ruby/payment_method.rb
+++ lib/spreedly-core-ruby/payment_method.rb
@@ -129,7 +129,8 @@ module SpreedlyCore
           :currency_code => currency,
           :ip => options[:ip_address],
           :redirect_url => options[:redirect_url],
-          :callback_url => options[:callback_url]
+          :callback_url => options[:callback_url],
+          :merchant_name_descriptor => options[:merchant_name_descriptor]
         }
       }
       self.class.verify_post(path, :body => data,
